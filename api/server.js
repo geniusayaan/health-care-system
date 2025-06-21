32a9b482070b579200dc0141d9fdbe44614c4629
AYAAN MEHDI <mehdiayaan786@gmail.com>
2025-06-21 14:41:41 +0530

    chnging the serve.js as a servelss

--- api/server.js
+++ api/server.js
@@ -1,30 +1,20 @@
+// api/server.js
 import express from "express";
-import path from 'path';
-import { healthRoute } from "./routes/heath-ai-routes.js";
-import dotenv from "dotenv";
-import { fileURLToPath } from 'url';
 import cors from "cors";
-import serverless from "serverless-http";
+import dotenv from "dotenv";
+import { healthRoute } from "./routes/heath-ai-routes.js";
 
 dotenv.config();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
-app.use(express.static(path.join(__dirname, 'Frontend')));
-
 app.get("/", (req, res) => {
-  res.send("Welcome to Health AI API 🚑");
+  res.json({ message: "welcome" });
 });
-
 
 app.use("/api/health", healthRoute);
 
-
-export const handler = serverless(app);
+export default app; 
